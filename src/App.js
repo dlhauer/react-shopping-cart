b8ea65402f129695093615104e6cdbdbfcedc101
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-12-09 13:30:40 -0800

    removeItem

--- src/App.js
+++ src/App.js
@@ -19,9 +19,15 @@ function App() {
 		setCart([...cart, item]);
 	};
 
+	const removeItem = id => {
+		setCart(cart.filter( item => {
+			return item.id !== id;
+		}))
+	}
+
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
-			<CartContext.Provider value={cart}>
+			<CartContext.Provider value={{ cart, removeItem }}>
 				<div className="App">
 					<Navigation />
 
